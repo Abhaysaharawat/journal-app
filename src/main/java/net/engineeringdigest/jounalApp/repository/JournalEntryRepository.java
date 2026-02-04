37ebddad5efc0461df359c13b1a8750c74478d1c
Abhaysaharawatgit <abhaysaharawat266@gmail.com>
2026-02-05 01:56:45 +0530

    Spring Boot Journal App

---
+++ src/main/java/net/engineeringdigest/jounalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,9 @@
+package net.engineeringdigest.jounalApp.repository;
+
+import net.engineeringdigest.jounalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository <JournalEntry, ObjectId> {
+
+}
---
+++ src/test/java/net/engineeringdigest/jounalApp/journalapp/JournalappApplicationTests.java
@@ -0,0 +1,11 @@
+package net.engineeringdigest.jounalApp.journalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalappApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
